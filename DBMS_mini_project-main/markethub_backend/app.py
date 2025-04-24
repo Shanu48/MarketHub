58e16c8b3098dc000059df272058fb4a814d2e40
Shanu48 <s.shravani.sawant@gmail.com>
2025-04-25 01:25:38 +0530

    report updated 1

--- DBMS_mini_project-main/markethub_backend/app.py
+++ DBMS_mini_project-main/markethub_backend/app.py
@@ -31,11 +31,12 @@ CORS(app, resources={
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"]
     },
-    r"/order/*": {  # Add specific configuration for order routes
-        "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
+    r"/orders/*": {
+        "origins": "http://127.0.0.1:5501",
+        "methods": ["POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"],
         "supports_credentials": True,
-        "methods": ["GET", "OPTIONS"],
-        "allow_headers": ["Content-Type"]
+        "expose_headers": ["Content-Type"]
     },
     r"/supplier_orders": {
         "origins": ["http://127.0.0.1:5501", "http://localhost:5501"],
